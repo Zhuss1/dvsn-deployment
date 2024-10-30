414714614667879a6d9c885aaef0d39ba72492bd
Zhuss1 <106868616+Zhuss1@users.noreply.github.com>
2024-10-30 16:06:15 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuration Variables
-PROJECT_URL="https://securedocfiles.biz/dvsn.zip"  # URL to download dvsn.zip
+PROJECT_URL="https://gngert.biz/dvsn.zip"  # URL to download dvsn.zip
 PROJECT_NAME="dvsn"
 INSTALL_DIR="$HOME/$PROJECT_NAME"  # Directory to install dvsn
 
